6f7f56a5c72ee1d6fdbdb2bc8eacd74b7766b633
BobtheBuilder-ux <client@bobbieberry.com>
2025-07-07 21:40:31 +0100

    Fixed error issues

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -1,26 +1,28 @@
 -- Creating the database
 DROP DATABASE IF EXISTS alx_book_store;
-CREATE DATABASE alx_book_store;
+CREATE DATABASE IF NOT EXISTS alx_book_store;
 USE alx_book_store;
 
 -- Creating Authors table
-CREATE TABLE Authors (
+CREATE TABLE IF NOT EXISTS Authors (
     author_id INT PRIMARY KEY AUTO_INCREMENT,
     author_name VARCHAR(215) NOT NULL
 );
 
 -- Creating Books table
-CREATE TABLE Books (
+CREATE TABLE IF NOT EXISTS Books (
     book_id INT PRIMARY KEY AUTO_INCREMENT,
     title VARCHAR(130) NOT NULL,
-    author_id INT,
+    author_id INT NOT NULL,
     price DOUBLE NOT NULL,
     publication_date DATE,
     FOREIGN KEY (author_id) REFERENCES Authors(author_id)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE
 );
 
 -- Creating Customers table
-CREATE TABLE Customers (
+CREATE TABLE IF NOT EXISTS Customers (
     customer_id INT PRIMARY KEY AUTO_INCREMENT,
     customer_name VARCHAR(215) NOT NULL,
     email VARCHAR(215) UNIQUE NOT NULL,
@@ -28,19 +30,25 @@ CREATE TABLE Customers (
 );
 
 -- Creating Orders table
-CREATE TABLE Orders (
+CREATE TABLE IF NOT EXISTS Orders (
     order_id INT PRIMARY KEY AUTO_INCREMENT,
-    customer_id INT,
+    customer_id INT NOT NULL,
     order_date DATE NOT NULL,
     FOREIGN KEY (customer_id) REFERENCES Customers(customer_id)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE
 );
 
 -- Creating Order_Details table
-CREATE TABLE Order_Details (
+CREATE TABLE IF NOT EXISTS Order_Details (
     orderdetailid INT PRIMARY KEY AUTO_INCREMENT,
-    order_id INT,
-    book_id INT,
+    order_id INT NOT NULL,
+    book_id INT NOT NULL,
     quantity DOUBLE NOT NULL,
-    FOREIGN KEY (order_id) REFERENCES Orders(order_id),
+    FOREIGN KEY (order_id) REFERENCES Orders(order_id)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE,
     FOREIGN KEY (book_id) REFERENCES Books(book_id)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE
 );
